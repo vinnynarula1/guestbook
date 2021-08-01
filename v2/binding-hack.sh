9bc6cf3d274d2ce56cf59e6b0c2198b3f864b2fc
vinnynarula1 <84547910+vinnynarula1@users.noreply.github.com>
2021-07-31 23:18:37 -0700

    updated file
    assigned name to tone analyzer

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tone_analyzer="Tone Analyzer-ek"
+tone_analyzer=Tone Analyzer-3l
 
 B64_URL=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.url -j | base64 -w 0)
 B64_APIKEY=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.apikey -j | base64 -w 0)
