90e154698dddc5f2378ffb8113134a6497961713
vinnynarula1 <84547910+vinnynarula1@users.noreply.github.com>
2021-07-31 23:21:41 -0700

    Update file

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tone_analyzer=Tone Analyzer-3l
+tone_analyzer= "Tone Analyzer-3l"
 
 B64_URL=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.url -j | base64 -w 0)
 B64_APIKEY=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.apikey -j | base64 -w 0)
@@ -10,7 +10,7 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: binding-tone
-  namespace: sn-labs-vinnynarula1
+  namespace: sn-labs-vinnynarula8
 type: Opaque
 data:
   url: $B64_URL
